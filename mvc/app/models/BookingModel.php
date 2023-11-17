e3041e4bf7befb1e984bc3bcd56a94ed58268cad
hishambali <hishamba23@gmail.com>
2023-11-17 11:52:17 +0200

    first edit by hisham

--- mvc/app/models/BookingModel.php
+++ mvc/app/models/BookingModel.php
@@ -1,6 +1,25 @@
 <?php 
-class ReservationModel {
-    
+class BookingModel {
+    private $db;
+    public function __construct($db) {
+        $this->db = $db;
+    }
+    public function getBooking ($id = null){
+        if (isset($id)) {
+            $this->db->where("id",$id);
+            return $this->db->get('booking');
+        }
+        else {
+            return $this->db->get('booking');
+        } 
+    }
+    public function addBooking($data){
+        return $this->db->insert('booking', $data);
+    }
+    public function UpdataBooking($data,$id){
+        $this->db->where('id',$id );
+        return $this->db->update('booking', $data);
+    }
 }
 
 
--- mvc/index.php
+++ mvc/index.php
@@ -1,11 +1,34 @@
 <?php
 
-
-define('BASE_PATH', '/mvc/');
+$request = $_SERVER['REQUEST_URI'];
+define('BASE_PATH', '/Tourist-office-reservations/mvc/');
 
 require_once  __DIR__.'/config/config.php';
-require_once  __DIR__.'/app/controllers/UserController.php';
-require_once  __DIR__.'/app/models/UserModel.php';
+require_once  __DIR__.'/app/controllers/BookingController.php';
+require_once  __DIR__.'/app/models/BookingModel.php';
 require_once  __DIR__.'/Lib/DB/MysqliDb.php';
+
+$config = require __DIR__.'/config/config.php';
+$db = new MysqliDb (
+    $config['db_host'],
+    $config['db_user'],
+    $config['db_pass'],
+    $config['db_name']
+);
+
+$model = new BookingModel($db);
+$controller = new BookingController($model);
+?>
+    <form method="post" action="add">
+
+        <button type="submit">add</button>
+    </form>
+    
+<?php
+switch ($request) {
+    
+    case BASE_PATH . "add":
+        $controller->addBooking();
+        break; }
 
 ?>
